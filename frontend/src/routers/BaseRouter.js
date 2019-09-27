886dcc81011975561fba9b7f2bd7efa7f5daa1a1
Alexandra Tarikova <tarikova@uchi.ru>
2019-09-27 18:21:30 +0300

    add links

--- frontend/src/routers/BaseRouter.js
+++ frontend/src/routers/BaseRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, withRouter, Redirect , NavLink} from 'react-router-dom';
 import MainPage from '../pages/MainPage/index';
 import LoginPage from '../pages/LoginPage/index';
 import GoalPage from '../pages/GoalPage/index';
