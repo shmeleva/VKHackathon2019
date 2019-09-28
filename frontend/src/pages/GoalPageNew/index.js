36f79d281a3b5a216e871763f60fa96386ad1ca4
Mary <mariyahaak@gmail.com>
2019-09-28 15:09:47 +0300

    goal page

--- frontend/src/pages/GoalPageNew/index.js
+++ frontend/src/pages/GoalPageNew/index.js
@@ -11,21 +11,37 @@ const GoalPageNew = props => {
       <Header pageName="Создание цели"/>
       <div className="GoalPageNew__inner page-content">
         <img className="GoalPageNew__image" src={image}/>
-        <label className="field-label">Цель</label>
-        <input 
-          className="text-field" 
+
+        <form className="GoalPageNew__form">
+
+          <label className="field-label">Цель</label>
+          <input className="text-field"
+                 type="text" 
+                 placeholder="Пройти 10 000 шагов">
+          </input>
+
+          <label className="field-label">Срок (кол-во дней)</label>
+          <input className="text-field"
           type="text" 
-          placeholder="Пройти 10 000 шагов">
-        </input>
-        <label className="field-label">Срок</label>
-        <label className="field-label">Периодичность</label>
-        <br/>
-        <Link className="primary-button primary-button--bright" to={"/main"}>
-          Отменить
-        </Link>
-        <Link className="primary-button" to={"/goal/100"}>
-          Создать
-        </Link>
+          placeholder="30">
+          </input>
+          <span className="text-field__helper text-field__helper--active">Неделя</span>
+          <span className="text-field__helper">2 недели</span>
+          <span className="text-field__helper">21 день</span>
+          <span className="text-field__helper">30 дней</span>
+          <span className="text-field__helper">90 дней</span>
+
+          <label className="field-label">Периодичность</label>
+
+          <Link className="primary-button" to={"/goal/100"}>
+            Создать
+          </Link>
+          <Link className="primary-button primary-button--bright" to={"/main"}>
+            Отменить
+          </Link>
+
+        </form>
+
       </div>
     </div>
   )
