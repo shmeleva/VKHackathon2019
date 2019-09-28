1caa61a8c30586b3b977ce88bf862890d50de1fa
Mary <mariyahaak@gmail.com>
2019-09-28 18:14:42 +0300

    field with helpers component

---
+++ frontend/src/components/FieldWithHelpers/FieldWithHelpers.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const FieldWithHelpers = props => {    
+  return (
+    <div className="FielsWithHelpers">
+      <input 
+        className="text-field"
+        type="text" 
+        placeholder="Введите количество дней"
+        value={props.value || ''}
+        onChange={(e)=>{
+          props.handleChange(e.target.value);
+          e.target.focus();
+        }}
+        key="FieldWithHelpers"
+      />
+
+      {Object.keys(props.helpers).map((key) => (
+        <span
+          onClick={(e)=>{
+            const newVal = props.helpers[key];
+            props.handleChange(newVal);
+          }} 
+          className={"text-field__helper "+(
+            Number(props.helpers[key]) === Number(props.value) && "text-field__helper--active"
+          )}
+          key={props.helpers[key]}
+        >
+        {key}
+        </span>
+      ))}
+    </div>
+  )
+}
+
+export default FieldWithHelpers;
--- frontend/src/pages/GoalPageNew/index.js
+++ frontend/src/pages/GoalPageNew/index.js
@@ -3,48 +3,68 @@ import { Link } from 'react-router-dom';
 import './styles.scss';
 import Header from '../../components/Header/Header';
 import image from './girl.jpg';
+import FieldWithHelpers from '../../components/FieldWithHelpers/FieldWithHelpers';
 
-const GoalPageNew = props => {
-  console.log("-------GoalPageNew: ", props);
-  return (
-    <div className="GoalPageNew">
-      <Header pageName="Создание цели"/>
-      <div className="GoalPageNew__inner page-content">
-        <img className="GoalPageNew__image" src={image}/>
+class GoalPageNew extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      period: null
+    };
+    this.handlePeriodChange = this.handlePeriodChange.bind(this);
+  }
 
-        <form className="GoalPageNew__form">
+  handlePeriodChange(value) {
+    this.setState(() => ({
+      period: value
+    }));
+  };
 
-          <label className="field-label">Цель</label>
-          <input className="text-field"
-                 type="text" 
-                 placeholder="Пройти 10 000 шагов">
-          </input>
+  render() {
+    return (
+      <div className="GoalPageNew">
+        <Header pageName="Создание цели"/>
+        <div className="GoalPageNew__inner page-content">
+          <img className="GoalPageNew__image" src={image}/>
 
-          <label className="field-label">Срок (кол-во дней)</label>
-          <input className="text-field"
-          type="text" 
-          placeholder="30">
-          </input>
-          <span className="text-field__helper text-field__helper--active">Неделя</span>
-          <span className="text-field__helper">2 недели</span>
-          <span className="text-field__helper">21 день</span>
-          <span className="text-field__helper">30 дней</span>
-          <span className="text-field__helper">90 дней</span>
+          <form className="GoalPageNew__form">
 
-          <label className="field-label">Периодичность</label>
+            <label className="field-label">Цель</label>
+            <input className="text-field"
+                  type="text" 
+                  placeholder="Пройти 10 000 шагов">
+            </input>
 
-          <Link className="primary-button" to={"/goal/100"}>
-            Создать
-          </Link>
-          <Link className="primary-button primary-button--bright" to={"/main"}>
-            Отменить
-          </Link>
+            <label className="field-label">Срок</label>
+            <FieldWithHelpers
+              value={this.state.period}
+              handleChange={this.handlePeriodChange}
+              helpers={
+                {
+                  'Неделя': 7,
+                  '2 недели': 14,
+                  '21 день': 21,
+                  '30 дней': 30,
+                  '90 дней': 90
+                }
+              }
+            />
 
-        </form>
+            <label className="field-label">Периодичность</label>
 
+            <Link className="primary-button" to={"/goal/100"}>
+              Создать
+            </Link>
+            <Link className="primary-button primary-button--bright" to={"/main"}>
+              Отменить
+            </Link>
+
+          </form>
+
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default GoalPageNew;
