420d7d1ad0199f2131001f84ca8aefb5a30ea3c5
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-09-28 23:37:23 +0300

    Use setUTCHours

--- backend/src/utils/dates.ts
+++ backend/src/utils/dates.ts
@@ -1,3 +1,3 @@
 export const dateWithoutHours = (iso8601: string): Date => {
-    return new Date(new Date(iso8601).setHours(0, 0, 0, 0));
+    return new Date(new Date(iso8601).setUTCHours(0, 0, 0, 0));
 };
