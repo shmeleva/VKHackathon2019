def466f4304b6cca76ebaa321d33695ca4a75bb0
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-09-29 06:46:54 +0300

    Use axios_url_herokuapp

--- frontend/src/js-variables.js
+++ frontend/src/js-variables.js
@@ -1,6 +1,6 @@
 const axios_url_local = 'http://localhost:3000';
 const axios_url_herokuapp = 'https://orbi-habits-api.herokuapp.com';
 
-const axios_url = axios_url_local;
+const axios_url = axios_url_herokuapp;
 
-export {axios_url};
+export { axios_url };
